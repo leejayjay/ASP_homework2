94e1e6217c618f357bf2fa4658daecc3a872e90b
leejay <1103137221@gm.kuas.edu.tw>
2017-03-31 14:26:07 +0800

    加入專案檔案。

---
+++ ASP_homework2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP_homework2.MvcApplication" Language="C#" %>
---
+++ ASP_homework2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_homework2.Startup))]
+namespace ASP_homework2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
