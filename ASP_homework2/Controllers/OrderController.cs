df0816ccce73d3ccfc3bae442caf1d1d1d9ebd3b
leejay <1103137221@gm.kuas.edu.tw>
2017-04-02 14:38:22 +0800

    第一章

---
+++ ASP_homework2/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_homework2.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            Models.OrderService orderservice = new Models.OrderService();
+            var order = orderservice.GetOrderById("111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.custName = order.CustName;
+            return View();
+        }
+    }
+}
